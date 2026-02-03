c24c0042614b01bf73a0899677b23b5f892e8c88
Shubham-0402 <shubhamv0436@gmail.com>
2026-02-03 11:23:33 +0530

    first commit

--- num guess.py
+++ num guess.py
@@ -1,10 +1,10 @@
 import random
 
-secret_number = random.randint(1, 10)
+secret_number = random.randint(1, 100)
 attempts = 0
 
 print("🎮 Welcome to the Number Guessing Game!")
-print("I am thinking of a number between 1 and 10.")
+print("I am thinking of a number between 1 and 100.")
 
 while True:
     guess = int(input("Enter your guess: "))
